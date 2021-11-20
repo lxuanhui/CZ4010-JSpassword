128f584c5ad84c14b5781942ec45fa381fb6f79e
shxnying <GOHS0100@e.ntu.edu.sg>
2021-11-21 05:59:16 +0800

    Settings change

--- cz4010/settings.py
+++ cz4010/settings.py
@@ -137,9 +137,9 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 
 
